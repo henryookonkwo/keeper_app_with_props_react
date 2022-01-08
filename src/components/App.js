2ff61082cbad7c0c31f3a6be7b1d1d537e3e901d
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2022-01-08 00:19:34 -0500

    use props and maps to update Notes

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+console.log(notes);
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {/* <Note /> */}
+      {notes.map((NodeItem) => (
+        <Note
+          key={NodeItem.key}
+          title={NodeItem.title}
+          content={NodeItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
